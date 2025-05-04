efb40d8b6274ca1867dcdabfc9d645ea1e1bfbd2
Juan Celis <juanm.celisr@gmail.com>
2025-05-04 18:10:42 -0500

    [Validators] feat(Added full Update models and FluentValidation rules for main entities (Address, Owner, Property)
    Included corresponding FluentValidation validators to ensure data integrity and business rules are respected before persistence.

---
+++ Million/Million.Application/Validators/Owner/UpdateOwnerModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Million.Application.Database.Commands.Owner.UpdateOwnerCommand;
+
+namespace Million.Application.Validators.Owner
+{
+    public class UpdateOwnerModelValidator : AbstractValidator<UpdateOwnerModel>
+    {
+        public UpdateOwnerModelValidator()
+        {
+            RuleFor(x => x.IdOwner).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Birthday).NotEmpty().LessThan(DateTime.UtcNow);
+            RuleFor(x => x.Photo).NotEmpty().MaximumLength(200);
+            RuleFor(x => x.IdAddress).NotEmpty();
+        }
+    }
+
+}
